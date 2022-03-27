cc676924c636e6051dc52d688c2a70e309853302
Nikhil Mehta <nikhil.001mehta@gmail.com>
2022-03-27 10:20:07 +0530

    fix: added short desc for root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -8,13 +8,7 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "mono",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A cli that works seamlessly and consistently across all your projects"
 }
 
 func Execute() {
