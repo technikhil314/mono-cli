4342495a108ed9531cd4fa6a34e570a6de64ed05
Nikhil Mehta <nikhil.001mehta@gmail.com>
2022-03-28 15:09:36 +0530

    feat: added support for maven, gradle and go modules

--- cmd/install.go
+++ cmd/install.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/technikhil314/mono-cli/utils"
@@ -10,7 +9,6 @@ import (
 )
 
 var infoStyle = chalk.Yellow.NewStyle().WithBackground(chalk.Black)
-var cwd, cwdErr = os.Getwd()
 
 var installCmd = &cobra.Command{
 	Use:   "install",
@@ -20,53 +18,46 @@ based on presence of package manager lock files. and will use the same package m
 if it is not able to detect then it will fallback to npm as pacakge manager	
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		executeMonorepoManagers()
-		executePackageManagers()
-		executeDefault()
+		runtime := utils.DetectRuntime()
+		println(runtime)
+		packageManager := utils.DetectPackageManager(runtime)
+		executeCommand(packageManager)
 	},
 }
 
-func executeMonorepoManagers() {
-	monorepoManagerFile := fmt.Sprint(cwd, "/lerna.json")
-	_, statErr := os.Stat(monorepoManagerFile)
-	if statErr == nil {
+func executeCommand(packageManager string) {
+	println(packageManager)
+	switch packageManager {
+	case "LERNA":
 		fmt.Printf("%s%s", infoStyle, "running lerna bootstrap\n")
 		utils.ExecuteCommand("npx", "lerna", "bootstrap")
-	}
-}
-
-func executePackageManagers() {
-	lockfilePath := fmt.Sprint(cwd, "/package-lock.json")
-	_, statErr := os.Stat(lockfilePath)
-	if statErr == nil {
+		break
+	case "NPM":
 		fmt.Printf("%s%s", infoStyle, "running npm install\n")
 		utils.ExecuteCommand("npm", "install")
-	}
-	lockfilePath = fmt.Sprint(cwd, "/yarn.lock")
-	_, statErr = os.Stat(lockfilePath)
-	if statErr == nil {
+		break
+	case "YARN":
 		fmt.Printf("%s%s", infoStyle, "running yarn install\n")
 		utils.ExecuteCommand("yarn", "install")
-	}
-	lockfilePath = fmt.Sprint(cwd, "/pnpm-lock.yaml")
-	_, statErr = os.Stat(lockfilePath)
-	if statErr == nil {
+		break
+	case "PNPM":
 		fmt.Printf("%s%s", infoStyle, "running pnpm install\n")
 		utils.ExecuteCommand("pnpm", "install")
+		break
+	case "MAVEN":
+		fmt.Printf("%s%s", infoStyle, "running maven install\n")
+		utils.ExecuteCommand("mvn", "install", "-Dmaven.test.skip.exec", "-Dmaven.test.skip=true")
+		break
+	case "GRADLE":
+		fmt.Printf("%s%s", infoStyle, "running gradle install\n")
+		utils.ExecuteCommand("gradle", "build")
+		break
+	case "GOMOD":
+		fmt.Printf("%s%s", infoStyle, "running go mod tidy\n")
+		utils.ExecuteCommand("go", "mod", "tidy")
+		break
 	}
 }
-
-func executeDefault() {
-	packageManagerFile := fmt.Sprint(cwd, "/package.json")
-	_, statErr := os.Stat(packageManagerFile)
-	if statErr == nil {
-		fmt.Println("%s%s", infoStyle, "No lock file found. Running npm install")
-		utils.ExecuteCommand("npm", "install")
-	}
-	fmt.Printf("%sNo relevant files found in %s\n", chalk.Red, cwd)
-	os.Exit(1)
-}
-
 func init() {
 	rootCmd.AddCommand(installCmd)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -7,6 +7,8 @@ import (
 	"os/exec"
 )
 
+var cwd, cwdErr = os.Getwd()
+
 func ExecuteCommand(cmdName string, args ...string) {
 	out, err := exec.Command(cmdName, args...).Output()
 	if err != nil {
@@ -15,4 +17,76 @@ func ExecuteCommand(cmdName string, args ...string) {
 	}
 	fmt.Println(string(out))
 	os.Exit(0)
+}
+
+func DetectRuntime() string {
+	var runtime = ""
+	packageManagerFile := fmt.Sprint(cwd, "/package.json")
+	_, statErr := os.Stat(packageManagerFile)
+	if statErr == nil {
+		runtime = "NODE"
+	}
+	packageManagerFile = fmt.Sprint(cwd, "/pom.xml")
+	_, statErr = os.Stat(packageManagerFile)
+	if statErr == nil {
+		runtime = "JAVA"
+	}
+	packageManagerFile = fmt.Sprint(cwd, "/build.gradle")
+	_, statErr = os.Stat(packageManagerFile)
+	if statErr == nil {
+		runtime = "JAVA"
+	}
+	return runtime
+}
+
+func DetectPackageManager(runtime string) string {
+	var packageManager string = ""
+	switch runtime {
+	case "JAVA":
+		packageManagerFile := fmt.Sprint(cwd, "/pom.xml")
+		println(packageManagerFile)
+		_, statErr := os.Stat(packageManagerFile)
+		if statErr == nil {
+			packageManager = "MAVEN"
+		}
+		packageManagerFile = fmt.Sprint(cwd, "/build.gradle")
+		_, statErr = os.Stat(packageManagerFile)
+		if statErr == nil {
+			packageManager = "GRADLE"
+		}
+		break
+	case "NODE":
+		lockfilePath := fmt.Sprint(cwd, "/package-lock.json")
+		_, statErr := os.Stat(lockfilePath)
+		if statErr == nil {
+			packageManager = "NPM"
+		}
+		lockfilePath = fmt.Sprint(cwd, "/yarn.lock")
+		_, statErr = os.Stat(lockfilePath)
+		if statErr == nil {
+			packageManager = "YARN"
+		}
+		lockfilePath = fmt.Sprint(cwd, "/pnpm-lock.yaml")
+		_, statErr = os.Stat(lockfilePath)
+		if statErr == nil {
+			packageManager = "PNPM"
+		}
+		monorepoManagerFile := fmt.Sprint(cwd, "/lerna.json")
+		_, statErr = os.Stat(monorepoManagerFile)
+		if statErr == nil {
+			packageManager = "LERNA"
+		}
+		break
+	case "GO":
+		packageManagerFile := fmt.Sprintf(cwd, "/go.mod")
+		_, statErr := os.Stat(packageManagerFile)
+		if statErr == nil {
+			packageManager = "GOMOD"
+		}
+		break
+	}
+	if packageManager == "" {
+		packageManager = runtime
+	}
+	return packageManager
 }
